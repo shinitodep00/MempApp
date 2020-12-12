2e1e111b54a666429c1cef11e43cfac7a38494e7
shin.ito.dep00 <shin.ito.dep@gmail.com>
2020-12-12 21:36:05 +0900

    Add navigation to sign up screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
-  StyleSheet, View, Text, TextInput, TouchableHighlight,
+  StyleSheet, View, Text, TextInput, TouchableHighlight, TouchableOpacity,
 } from 'react-native';
 import firebase from 'firebase';
+import { NavigationActions, StackActions } from 'react-navigation';
 
 class LoginScreen extends React.Component {
   state = {
@@ -13,15 +14,23 @@ class LoginScreen extends React.Component {
   // eslint-disable-next-line
   handleSubmit() {
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then(({ user }) => {
-        console.log('success!', user);
-        this.props.navigation.navigate('Home');
+      .then(() => {
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Home' }),
+          ],
+        });
+        this.props.navigation.dispatch(resetAction);
       })
-      .catch((error) => {
-        console.log('error', error);
+      .catch(() => {
       });
 
     // Log in!
+  }
+
+  handlePress() {
+    this.props.navigation.navigate('Signup');
   }
 
   render() {
@@ -48,6 +57,10 @@ class LoginScreen extends React.Component {
         <TouchableHighlight style={styles.button} onPress={this.handleSubmit.bind(this)} underlayColor="#C70F66">
           <Text style={styles.buttonTitle}>ログインする</Text>
         </TouchableHighlight>
+
+        <TouchableOpacity style={styles.signup} onPress={this.handlePress.bind(this)}>
+          <Text style={styles.signupText}>メンバー登録する</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -85,6 +98,13 @@ const styles = StyleSheet.create({
   buttonTitle: {
     color: '#fff',
     fontSize: 18,
+  },
+  signup: {
+    marginTop: 16,
+    alignSelf: 'center',
+  },
+  signupText: {
+    fontSize: 16,
   },
 });
 
--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -16,11 +16,10 @@ class MemoCreateScreen extends React.Component {
       body: this.state.body,
       created_on: new Date(),
     })
-      .then((docRef) => {
-        console.log(docRef.id);
+      .then(() => {
+        this.props.navigation.goBack();
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(() => {
       });
   }
 
